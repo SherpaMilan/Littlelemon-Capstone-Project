0eaa1d910abb5cbbc3bdd53442c27301e9a7cc27
SherpaMilan <msherpa574@icloud.com>
2024-01-09 23:49:53 +1100

    what a frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import About from "./components/About";
-import Footer from "./components/Footer";
-import HeroSection from "./components/HeroSection";
-import Highlights from "./components/Highlights";
-import Testimonials from "./components/Testimonials";
+import HeroSection from "./components/Hero-Section";
 import "./App.css";
-import { Header } from "./components/Header";
-
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <>
       <HeroSection />
-      <Highlights />
-      <Testimonials />
-      <About />
-      <Footer />
-      {/* <Example /> */}
-    </div>
+    </>
   );
 }
 
