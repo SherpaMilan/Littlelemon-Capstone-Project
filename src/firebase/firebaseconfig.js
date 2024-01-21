d555b8d9146142551d09000e601b89c625a6ae85
SherpaMilan <msherpa574@icloud.com>
2024-01-21 16:38:09 +1100

    changes on firebaseconfig

--- src/firebase/firebaseconfig.js
+++ src/firebase/firebaseconfig.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APPID,
 };
 
-console.log(firebaseConfig);
+// console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
