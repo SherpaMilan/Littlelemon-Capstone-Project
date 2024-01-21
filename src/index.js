6574dca5fb1367f93663515c691a34e87eed92f8
SherpaMilan <msherpa574@icloud.com>
2024-01-21 17:00:09 +1100

    updating react router warning

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // Import createRoot
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = document.getElementById("root");
 
-ReactDOM.render(
+// Use createRoot instead of ReactDOM.render
+const rootInstance = createRoot(root);
+
+// Render the App component inside createRoot
+rootInstance.render(
   <Router>
     <App />
-  </Router>,
-  root
+  </Router>
 );
